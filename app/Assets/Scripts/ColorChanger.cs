5c9c787551992ccc9a90d48cf313cfbe00e5815f
Mike Judge <mikelovesrobots@gmail.com>
2013-04-26 12:49:56 -0700

    Make ColorChanger color name agnostic

--- app/Assets/Scripts/ColorChanger.cs
+++ app/Assets/Scripts/ColorChanger.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class ColorChanger : MonoBehaviour {
     public Renderer[] Renderers;
+    public string ColorName = "_TintColor";
 
     public void ChangeColor(Color color) {
         foreach (var renderer in Renderers) {
-            renderer.material.color = color;
+            renderer.material.SetColor(ColorName, color);
         }
     }
 }
